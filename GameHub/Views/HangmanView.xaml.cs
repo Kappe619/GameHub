59f784fe91270ea3032e7b9e3124508ef3d05613
Britta <kappe619@gmail.com>
2023-10-08 02:24:48 +0200

    TEST: hangman images

--- GameHub/Views/HangmanView.xaml.cs
+++ GameHub/Views/HangmanView.xaml.cs
@@ -5,5 +5,15 @@ public partial class HangmanView : ContentView
 	public HangmanView()
 	{
 		InitializeComponent();
+
 	}
+
+	public string path1 = "Images/hangmanpicb.png";
+    public string path2 = "Images/hangmanpicc.png";
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+		image1.Source = new FileImageSource { File = path1 };
+    }
 }
+
